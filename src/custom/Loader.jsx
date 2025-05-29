45eb84660c997a44bc6fa2fbea37325f32cb9302
Harshit_Parmar <parmarharshit441@gmail.com>
2025-05-29 21:42:25 +0530

    chore: receive color property as a prop

--- src/custom/Loader.jsx
+++ src/custom/Loader.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Loader as LoaderIcon } from "lucide-react";
 
-const Loader = () => {
+const Loader = ({ color }) => {
   return (
     <div className="flex items-center justify-center w-full h-full">
       <LoaderIcon
-        className="animate-spin text-black"
+        className={`animate-spin text-${color}`}
         size={36}
         strokeWidth={2.5}
       />
